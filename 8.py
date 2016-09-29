89258e531fa1733eda48f8f7ad5cda7a9294e622
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-29 21:21:57 +0300

    1

--- 8.py
+++ 8.py
@@ -1,1 +1,11 @@
 __author__ = 'student'
+n=int(input())
+A=[]
+k=0
+for i in range(n):
+    A.extend(list(map(int, input().split())))
+t = int (input())
+for i in range(0, len(A), 2):
+    if A[i]<=t and A[i+1]>=t:
+        k+=1
+print(k)
